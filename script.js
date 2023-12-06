39737bbaddbb1037eb8b08c3c68b6a5e01f3b1b6
fabriciacosta <fabriciacfa@gmail.com>
2023-12-06 20:04:40 -0300

    primeiro desafio de logica

---
+++ script.js
@@ -0,0 +1,14 @@
+let _nome = "Ano novo está chegando!!";
+console.log(_nome)
+
+let nome = "Mariah Carey";
+console.log(nome)
+
+let idade = 54;
+console.log(idade)
+
+let AnoAtual = 2023;
+console.log(AnoAtual)
+
+console.log(AnoAtual-=idade)
+
