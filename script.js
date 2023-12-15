3c70857a3809db85f2661ac4faca184f61417101
fabriciacosta <fabriciacfa@gmail.com>
2023-12-15 11:39:43 -0300

    Completei o desafio

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 let _nome = "Ano novo está chegando!!";
-console.log(_nome)
+console.log(_nome);
 
 let nome = "Mariah Carey";
-console.log(nome)
+console.log(nome);
 
 let idade = 54;
-console.log(idade)
+console.log(idade);
 
-let AnoAtual = 2023;
-console.log(AnoAtual)
+let anoAtual = 2023;
+console.log(anoAtual);
 
-console.log(AnoAtual-=idade)
+console.log(anoAtual -= idade);
+
+let quartaFeira = true;
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
+
+let _idade = 16;
+
+if (_idade >= 18) {
+    console.log("Você é maior de idade, poderá curtir o show de Robertos Carlos com participação de Mariah Carey");
+} else{
+    console.log("Sinto muito, mas assistirá da tv globo.");
+}
+
+let niver = "Outubro";
+
+if(niver == "Agosto" || niver == "Dezembro" || niver == "Junho") {
+    console.log("Uma ou mais opções estão corretas.");
+} else{
+    console.log(` Algo de errado não está certo, o mês digitado foi o mês de ${niver}`);
+}
 
